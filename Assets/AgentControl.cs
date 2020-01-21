7ca1a58eb0b4a2e48c473eaafebe95aabc7aef6d
yufei.shao <yufei.shao@de.bosch.com>
2020-01-21 22:38:38 +0100

    First commit

---
+++ Assets/AgentControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentControl : MonoBehaviour
+{
+    public Camera cam;
+    public Transform home;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(home.position);
+    }
+
+    // Update is called once per frame
+    //  void Update()
+    // {
+
+    // }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //MOVE OuR Agent
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+}
